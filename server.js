5138af9e0538e477d4eedf357632807ed8c7fd55
Richard Cândido <rich_dacan@protonmail.com>
2022-05-17 10:29:15 -0300

    feat:sparkles: new routes

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 app.db = router.db;
 const rules = auth.rewriter({
-  users: 600,
+  users: 664,
   products: 644,
-  providers: 644,
-  supplies: 644
+  providers: 600,
+  supplies: 600,
+  finances: 600,
+  orders: 600,
+  tickets: 600
 });
 app.use(cors());
 app.use(rules);
